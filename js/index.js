4489eb76925e3fadc42bbbec5a838c2fd1c673fc
Tehmo3 <jasper.b.miles@gmail.com>
2017-01-20 22:13:22 +1100

    typing + fixing bug

---
+++ js/index.js
@@ -0,0 +1,10 @@
+var main = function () {
+    $(function() {
+        $(".typeSpace").typed({
+            strings: ["Hi."]
+            typespeed: 0
+        });
+    });
+}
+
+$(document).ready(main);
