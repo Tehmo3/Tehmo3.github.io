b99745598271fa01a791c350b2c8df29bfa75d74
Tehmo3 <jasper.b.miles@gmail.com>
2017-01-20 22:20:46 +1100

    wait before execute + actually can see text + blinking cursor

--- js/index.js
+++ js/index.js
@@ -1,10 +1,20 @@
 var main = function () {
+    wait(5000);
     $(function() {
         $(".typeSpace").typed({
             strings: ["Hi."],
             typespeed: 0
         });
     });
+}
+
+
+function wait(ms){
+   var start = new Date().getTime();
+   var end = start;
+   while(end < start + ms) {
+     end = new Date().getTime();
+  }
 }
 
 $(document).ready(main);
