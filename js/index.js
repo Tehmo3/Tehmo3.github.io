4584cd739b103968ab464593745f5e04690360fd
Tehmo3 <jasper.b.miles@gmail.com>
2017-01-21 01:52:21 +1100

    updates

--- js/index.js
+++ js/index.js
@@ -1,7 +1,7 @@
 var main = function () {
     $(function() {
         $(".typeSpace").typed({
-            strings: ["^1000 Hey! ^2000 My name is <b>Jasper Miles</b>, and I'm a full stack web developer from Melbourne, Australia."],
+            strings: ["^1000 <p>Hey! ^2000 My name is <b>Jasper Miles</b>, and I'm a full stack web developer from Melbourne, Australia.</p>"],
             typespeed: 1,
             showCursor: false
         });
