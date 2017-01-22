ede9bbc58a82eceaa208315a4b8ca77228f4d041
Tehmo3 <jasper.b.miles@gmail.com>
2017-01-22 19:09:25 +1100

    slide animation

--- js/index.js
+++ js/index.js
@@ -1,5 +1,6 @@
 var main = function () {
     $(window).scrollTo(".animate", 10);
+    $(window).on('scroll resize', check_if_in_view);
     $(function() {
         $(".typeSpace").typed({
             strings: ["<p>Hey! ^2000 My name is <b>Jasper Miles</b>, and I'm a full stack web developer from Melbourne, Australia.</p>"],
@@ -32,6 +33,27 @@ function wait(ms){
    while(end < start + ms) {
      end = new Date().getTime();
   }
+}
+
+function check_if_in_view() {
+  var $animation_elements = $('.animation_element');
+  var $window = $(window);
+  var window_height = $window.height();
+  var window_top_position = $window.scrollTop();
+  var window_bottom_position = (window_top_position + window_height);
+
+  $.each($animation_elements, function() {
+    var $element = $(this);
+    var element_height = $element.outerHeight();
+    var element_top_position = $element.offset().top;
+    var element_bottom_position = (element_top_position + element_height);
+
+    //check to see if this current container is within viewport
+    if ((element_bottom_position >= window_top_position) &&
+        (element_top_position <= window_bottom_position)) {
+      $element.addClass('in-view');
+    }
+  });
 }
 
 var scrollToPortfolio = function () {
